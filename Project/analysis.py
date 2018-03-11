23eb9d852a2c019ce626d8a68ad0348c35a8e8d5
John Karasinski <karasinski@gmail.com>
2018-03-11 16:10:09 -0700

    almost a perfect match

--- Project/analysis.py
+++ Project/analysis.py
@@ -127,37 +127,42 @@ for i in range(7):
   print(latex(simplify(T[i].D[1, 0])))
   print('\n')
 
-SHOULDER, WRIST, ELBOW = 1, 1, 1
 
-theta1 = beta
+def IK(SHOULDER=1, WRIST=1, ELBOW=1,
+       a3=2.3, a4=2.3, d1=0.65, d2=0.3,
+       d3=0.9, d6=0.3, d7=0.65,
+       beta=rad(60),
+       n_x=0.8021, n_y=-0.5859, n_z=-0.1154,
+       o_x=0.1217, o_y= 0.3495, o_z=-0.9290,
+       a_x=0.5846, a_y= 0.7311, a_z=0.3517,
+       p_x=2.4790, p_y=-2.4734, p_z=-0.4927):
 
-h1 = (-o_z * d7 - d1 + p_z)
-q1 = ((o_x * d7 - p_x)*cos(beta) + (o_y * d7 - p_y)*sin(beta))
+  theta1 = beta
 
-theta2 = SHOULDER * acos(d3/(sqrt(h1**2 + q1**2))) + atan2(q1, h1)
+  h1 = -o_z * d7 - d1 + p_z
+  q1 = (o_x * d7 - p_x)*cos(beta) + (o_y * d7 - p_y)*sin(beta)
 
-theta6 = WRIST * acos(o_z*cos(theta2)-(o_x*cos(theta1) + o_y*sin(theta1))*sin(theta2))
+  theta2 = SHOULDER * acos(d3/(sqrt(h1**2 + q1**2))) + atan2(q1, h1) - pi
 
-theta7 = atan2(((n_x*cos(theta1) + n_y*sin(theta1))*sin(theta2) - n_z*cos(theta2))/sin(theta6),
-                  ((a_x*cos(theta1) + a_y*sin(theta1))*sin(theta2) - a_z*cos(theta2))/sin(theta6))
+  theta6 = WRIST * acos(o_z*cos(theta2)-(o_x*cos(theta1) + o_y*sin(theta1))*sin(theta2))
 
-X = -d7 * (o_z*sin(theta2)+cos(theta2)*(o_x*cos(theta1)+o_y*sin(theta1)))+(-d1+p_z)*sin(theta2)+cos(theta2)*(p_x*cos(theta1)+p_y*sin(theta1))
-Y = -d2 * d6 * (a_x*sin(theta1)-a_y*cos(theta1)) - d6*(n_x*sin(theta1)-n_y*cos(theta1))*sin(theta7) - d7*(o_x*sin(theta1)-o_y*cos(theta1)) + p_x*sin(theta1)-p_y*cos(theta1)
+  theta7 = -atan2(((n_x*cos(theta1) + n_y*sin(theta1))*sin(theta2) - n_z*cos(theta2))/sin(theta6),
+                  ((a_x*cos(theta1) + a_y*sin(theta1))*sin(theta2) - a_z*cos(theta2))/sin(theta6)) + pi/2
 
-theta4 = ELBOW * acos((X**2 + Y**2 - a3**2 - a4**2)/(2*a3*a4))
+  X = d6 * ((a_z * sin(theta2) + cos(theta2) * (a_x * cos(theta1) + a_y * sin(theta1))) * cos(theta7) - (n_z * sin(theta2) + cos(theta2) * (n_x * cos(theta1) + n_y * sin(theta1))) * sin(theta7)) - d7 * (o_z * sin(theta2) + cos(theta2) * (o_x * cos(theta1) + o_y * sin(theta1))) + (-d1 + p_z) * sin(theta2) + cos(theta2) * (p_x * cos(theta1) + p_y * sin(theta1))
+  Y = -d2 + d6 * ((a_x * sin(theta1) - a_y * cos(theta1)) * cos(theta7) - (n_x * sin(theta1) - n_y * cos(theta1)) * sin(theta7)) - d7 * (o_x * sin(theta1) - o_y * cos(theta1)) + p_x * sin(theta1) - p_y*cos(theta1)
 
-theta3 = atan2(Y*(a3 + a4*cos(theta4)) - X*a4*sin(theta4),
-               X*(a3 + a4*cos(theta4)) + Y*a4*sin(theta4))
+  theta4 = ELBOW * acos((X**2 + Y**2 - a3**2 - a4**2)/(2*a3*a4))
 
-theta5 = atan2((a_x*sin(theta1)-a_y*cos(theta1))*cos(theta7)-(n_x*sin(theta1)-n_y*cos(theta1))*sin(theta7),
-               (o_x*sin(theta1)-o_y*cos(theta1))/(sin(theta6))) - (theta3 + theta4)
+  theta3 = atan2(Y*(a3 + a4*cos(theta4)) - X*a4*sin(theta4),
+                 X*(a3 + a4*cos(theta4)) + Y*a4*sin(theta4))
 
-params = {a3:2.3, a4:2.3, d1:0.65, d2:0.3,d3:0.9,d6:0.3,d7:0.65, beta: rad(60), o_x: 0.1217, p_x: 2.4790, o_y: 0.3495, p_y: -2.4734, o_z: -0.9290, p_z: -0.4927, n_x: 0.8021, n_y: -0.5859, n_z: -0.1154, a_x: 0.5846, a_y: 0.7311, a_z: 0.3517}
+  theta5 = atan2((o_x*sin(theta1)-o_y*cos(theta1))/(sin(theta6)),
+                 (a_x*sin(theta1)-a_y*cos(theta1))*cos(theta7)-(n_x*sin(theta1)-n_y*cos(theta1))*sin(theta7)) - (theta3 + theta4)
 
-t1e = 1.04719755119660
-t2e = 2.78783357921311
-t3e = 0.647418062415987
-t4e = 1.35058161602201
-t5e = -0.140025071653727
-t6e = 0.729453686436415
+  thetas = []
+  for theta in [theta1, theta2, theta3, theta4, theta5, theta6, theta7]:
+    thetas.append(deg(theta).evalf())
 
+  thetas = Matrix(thetas)
+  return thetas
